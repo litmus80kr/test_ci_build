accb7ec84cc6898bb23661d8a16da2016dff48e5
wonsuk.lim <wonsuk.lim@navercorp.com>
2021-07-26 13:07:49 +0900

    소스코드 초기화

---
+++ app/src/main/java/com/nhn/android/androidcitest/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.nhn.android.androidcitest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
